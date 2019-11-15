7232f9ab1c0bc04849c035b9f511168010116583
Matheus Guimaraes <matheusguimr@gmail.com>
2019-11-15 19:06:02 -0300

    Add script to create SpotPer

---
+++ scripts/createDBSpotPer.sql
@@ -0,0 +1,35 @@
+-- Create database
+CREATE DATABASE spotper
+ON 
+	PRIMARY (
+		NAME = 'spotper',
+		FILENAME = 'C:\BDSpotPer\spotper.mdf',
+		SIZE = 5120KB,
+		FILEGROWTH = 30%
+	),
+
+	FILEGROUP spoter_fg01 (
+		NAME = 'spotper_001',
+		FILENAME = 'C:\BDSpotPer\spotper_001.ndf',
+		SIZE = 5120KB,
+		FILEGROWTH = 30%
+	), (
+		NAME = 'spotper_002',
+		FILENAME = 'C:\BDSpotPer\spotper_002.ndf',
+		SIZE = 5120KB,
+		FILEGROWTH = 20%
+	),
+
+	FILEGROUP spoter_fg02 (
+		NAME = 'spotper_003',
+		FILENAME = 'C:\BDSpotPer\spotper_003.ndf',
+		SIZE = 5120KB,
+		FILEGROWTH = 20%
+	)
+
+	LOG ON (
+		NAME = 'spotper_log',
+		FILENAME = 'C:\BDSpotPer\spotper_log.ldf',
+		SIZE = 5120KB,
+		FILEGROWTH = 20%
+	)
