a6d6ef3bf693fac2a739dee5f5175cd38d106c5a
Matheus Guimaraes <matheusguimr@gmail.com>
2020-04-06 23:55:53 -0300

    Rename file and add constructor

--- SpotPer/src/controller/ConnectDatabase.java
+++ SpotPer/src/controller/ConnectDatabase.java
@@ -2,32 +2,17 @@ package controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import javax.swing.JOptionPane;
 
 /*
  * Connect with database and return connection
  */
 
-public class DbConnection {
+public class ConnectDatabase {
     private Connection conn;
 
-    public DbConnection(Connection conn) {
-        this.conn = conn;
-    }
-
-    public Connection getConn() {
-        return conn;
-    }
-
-    public void setConn(Connection conn) {
-        this.conn = conn;
-    }
-
-    public static Connection getConnection() {
-        Connection conn = null;
+    public ConnectDatabase() {
         String userName = "sa";
         String password = "@admin123";
-        String database = "spotper";
         String url = "jdbc:sqlserver://localhost;database=spotper";
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -35,15 +20,10 @@ public class DbConnection {
             System.out.println("Connected to database");
         } catch (Exception ex) {
             ex.printStackTrace();
-            JOptionPane.showMessageDialog(null,
-                    "Failed connection with database",
-                    "Message",
-                    JOptionPane.ERROR_MESSAGE);
         }
-        return conn;
     }
 
-    public static void closeConnection(Connection conn) {
+    public void closeConnection() {
         try {
             conn.close();
             System.out.println("Connection to database closed");
@@ -53,8 +33,8 @@ public class DbConnection {
     }
 
     public static void main(String[] args) {
-        Connection co = DbConnection.getConnection();
+        ConnectDatabase databaseConn = new ConnectDatabase();
         System.out.println("test connection");
-        DbConnection.closeConnection(co);
+        databaseConn.closeConnection();
     }
 }
